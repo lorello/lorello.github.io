1c7120b7d2dd3dbca3f6597deee5f33ab545ea91
LoreLLo <lsalvadorini@cloud4wi.com>
2017-06-26 10:07:32 +0200

    fixed build command: urls in md are generated correctly

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ git checkout mkdocs
 
 BASEURL=https://github.com/lorello/lorello.github.io/commit
 
-git log --abbrev-commit --pretty=format:"* %an commit <$BASEURL/%H|%h> di %ar: %B" --after='$(date --date="1 month ago" +%Y-%m-%d)'| sed ':a;N;$!ba;s/\n\n/\n/g' > ./docs/changelog.md
+git log --abbrev-commit --pretty=format:"* %an commit [%h]($BASEURL/%H) di %ar: %B" --after='$(date --date="1 month ago" +%Y-%m-%d)'| sed ':a;N;$!ba;s/\n\n/\n/g' > ./docs/changelog.md
 
 git commit -m "Updated changelog" ./docs/changelog.md
 
